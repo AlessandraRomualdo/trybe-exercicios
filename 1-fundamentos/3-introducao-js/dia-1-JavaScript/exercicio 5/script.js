111ad10d1cfe305f1ce5da7d0fcf488b104c4234
AlessandraRomualdo <romualdo.alessandra23@gmail.com>
2023-01-23 14:41:54 -0300

    exercicio 5 feito

---
+++ 1-fundamentos/3-introducao-js/dia-1-JavaScript/exercicio 5/script.js
@@ -0,0 +1,18 @@
+
+//Crie um Switch/case
+
+let resultado = 'aprovada'
+
+switch(resultado){
+    case 'aprovada' :
+        console.log('Parabèns, você foi aprovado(a)')
+        break
+    case 'lista' :
+        console.log('Você está na nossa lista de espera')
+        break
+    case 'reprovada' :
+        console.log('Você foi reprovado(a)')
+        break
+        default:
+            console.log('Informação incorreta')            
+}
