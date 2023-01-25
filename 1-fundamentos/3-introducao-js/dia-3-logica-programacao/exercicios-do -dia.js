e185ad7a83106fc0d0d41469d0536e7c89cbf711
AlessandraRomualdo <romualdo.alessandra23@gmail.com>
2023-01-25 12:05:47 -0300

    exercicio de maior e menor palavra feito

--- 1-fundamentos/3-introducao-js/dia-3-logica-programacao/exercicios-do -dia.js
+++ 1-fundamentos/3-introducao-js/dia-3-logica-programacao/exercicios-do -dia.js
@@ -5,5 +5,33 @@
 let fatorial = 1
 for(let index = 10; index > 0; index -= 1){
     fatorial *= index   
+   
 }
 console.log(fatorial)
+
+// Utilize a estrutura de repetição for para desenvolver um algoritmo que seja capaz de inverter uma palavra. Por exemplo, a palavra “banana” seria invertida para “ananab”. Utilize a string abaixo como exemplo, depois troque por outras para verificar se seu algoritmo está funcionando corretamente.
+let word = 'Chocolate'
+let inverter = ''
+for(let index = 0; index < word.length; index += 1){
+    inverter += word[word.length - 1 - index]
+}
+console.log(inverter)
+
+//Utilize a estrutura de repetição for para escrever dois algoritmos: um que imprima no console a maior palavra desse array e outro que imprima a menor. Considere o número de caracteres de cada palavra.
+let array = ['java', 'javascript', 'python', 'html', 'css']
+let palavraMaior = array[0]
+let menorPalavra =array[0]
+for(let index = 0; index < array.length; index += 1){
+     
+        if(array[index].length > palavraMaior.length){
+            palavraMaior = array[index]
+        }
+}
+console.log(palavraMaior)
+
+for(let index = 0; index < array.length; index += 1){
+    if(array[index].length < menorPalavra.length){
+        menorPalavra = array[index]
+    }
+}
+console.log(menorPalavra)
