563b849e1db6c08c62aa0c7d069cf393c6c6e0da
AlessandraRomualdo <romualdo.alessandra23@gmail.com>
2023-01-25 15:57:14 -0300

    exercicios bonus

--- 1-fundamentos/3-introducao-js/dia-3-logica-programacao/bonus.js
+++ 1-fundamentos/3-introducao-js/dia-3-logica-programacao/bonus.js
@@ -1,4 +1,28 @@
 //Agora vamos trabalhar com algumas formas geométricas! Utilize a estrutura de repetição for para escrever os programas abaixo:
 //Escreva um programa que, dado um valor n qualquer, seja n > 1, imprima na tela um quadrado feito de asteriscos de lado de tamanho n. Por exemplo:
+/** 
+ /de um vanor maior que 1
+ /imprima o valor em linhas 
+ */
+let n = 4
+let linha = ''
 
+for(let index = 0; index < n; index +=1){
+    linha += '*'
+}
 
+for(let index = 0; index < n; index += 1){
+    console.log(linha)
+}
+
+/**
+ * Faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base.
+ */
+let v = 5
+let simbolo = '*'
+let newLine = ''
+
+for(let index = 0; index <= v; index +=1){
+    console.log(newLine)
+    newLine  += simbolo
+}
