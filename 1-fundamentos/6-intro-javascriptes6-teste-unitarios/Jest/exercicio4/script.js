15f42a28696d93d19e1833125cd65d5bbed19666
AlessandraRomualdo <romualdo.alessandra23@gmail.com>
2023-02-23 12:51:03 -0300

    exercicios de jest 4

---
+++ 1-fundamentos/6-intro-javascriptes6-teste-unitarios/Jest/exercicio4/script.js
@@ -0,0 +1,54 @@
+const techList = require('./techList.js');
+
+const techList = (array, string) => {
+  if (array === 0) return 'Vazio!';
+  let arrayOrdenado = array.sort();
+  let list = [];
+ for (let i of arrayOrdenado) {
+  let obj = {
+    tech: i,
+    name: string,
+  }
+  list.push(obj);
+  }
+ return list;
+};
+console.log(techList('','Lucas'))
+
+describe('Testa a função techList', () => {
+  it('Testa se a função techList é definida', () => {
+    expect(techList).toBeDefined();
+  });
+  it('Testa se techList é uma função', () => {
+    expect(typeof techList).toBe('function');
+  });
+  it('Lista com 5 tecnologias deve retornar uma lista de objetos ordenados', () => {
+    expect(techList(['React', 'Jest', 'HTML', 'CSS', 'JavaScript'], 'Lucas')).toEqual([
+      {
+        tech: 'CSS',
+        name: 'Lucas'
+      },
+      {
+        tech: 'HTML',
+        name: 'Lucas'
+      },
+      {
+        tech: 'JavaScript',
+        name: 'Lucas'
+      },
+      {
+        tech: 'Jest',
+        name: 'Lucas'
+      },
+      {
+        tech: 'React',
+        name: 'Lucas'
+      }
+    ]);
+  });
+  it('Lista com 0 tecnologias deve retornar uma mensagem de erro "Vazio!"', () => {
+    expect(techList([], 'Lucas')).toBe('Vazio!');
+  });
+});
+
+module.exports = techList;
