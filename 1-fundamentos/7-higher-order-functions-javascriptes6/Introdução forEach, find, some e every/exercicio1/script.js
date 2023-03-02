c3367999f35a371aa442d727ee11b41ace9ff4fb
AlessandraRomualdo <romualdo.alessandra23@gmail.com>
2023-03-02 13:46:24 -0300

    exercícios de some() e every()

--- 1-fundamentos/7-higher-order-functions-javascriptes6/Introdução forEach, find, some e every/exercicio1/script.js
+++ 1-fundamentos/7-higher-order-functions-javascriptes6/Introdução forEach, find, some e every/exercicio1/script.js
@@ -37,3 +37,54 @@ const musicas = [
 const encontrada = musicas.find((musica) => {
   musica.id === '31031685'
 })
+
+const pessoas = [
+  { nome: 'Ana', cargo: 'Analista' },
+  { nome: 'João', cargo: 'Gerência' },
+  { nome: 'Aline', cargo: 'Analista' },
+  { nome: 'Joana', cargo: 'Gerência' },
+];
+
+const verificaCargo = pessoas.some((pessoa) => pessoa.cargo === 'Gerência');
+
+console.log(verificaCargo); // true
+
+const listNames = ['Maria', 'Manuela', 'Jorge', 'Ricardo', 'Wilson'];
+
+const verifyFirstLetter = (letter, names) => names.some((name) => name[0] === letter);
+
+console.log(verifyFirstLetter('J', listNames)); // true
+console.log(verifyFirstLetter('X', listNames)); // false
+
+const grades = {
+  portugues: 'Aprovado',
+  matematica: 'Reprovado',
+  ingles: 'Aprovado',
+};
+
+const verifyGrades = Object.values(grades).every((grade) => grade === 'Aprovado'); // false
+
+console.log(verifyGrades);
+
+// 
+const nomes = ['Mateus', 'José', 'Ana', 'Cláudia', 'Bruna'];
+
+const hasName = (arr, name) => arr.some((nome) => nome === name);
+
+console.log(hasName(nomes, 'Ana'));
+console.log(hasName(nomes, 'Pedro'));
+
+//
+
+const people = [
+  { name: 'Mateus', age: 18 },
+  { name: 'José', age: 16 },
+  { name: 'Ana', age: 23 },
+  { name: 'Cláudia', age: 20 },
+  { name: 'Bruna', age: 19 },
+];
+
+const verifyAges = (arr, idade) => arr.every((idad) => idad.age >= idade);
+
+console.log(verifyAges(people, 18));
+console.log(verifyAges(people, 14));
