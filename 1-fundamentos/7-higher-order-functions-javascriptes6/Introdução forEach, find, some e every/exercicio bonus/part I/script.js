fe54b2f21ebbf7d306b63c0192e572563c2d4f1e
AlessandraRomualdo <romualdo.alessandra23@gmail.com>
2023-03-04 12:51:42 -0300

    função para calcular o gasto de mana e o dano do mago

--- 1-fundamentos/7-higher-order-functions-javascriptes6/Introdução forEach, find, some e every/exercicio bonus/part I/script.js
+++ 1-fundamentos/7-higher-order-functions-javascriptes6/Introdução forEach, find, some e every/exercicio bonus/part I/script.js
@@ -37,3 +37,21 @@ const damageWarrior = () => {
   return damage;
 }
 console.log(damageWarrior());
+// Crie uma função que retorne um objeto com duas chaves e dois valores contendo o dano e a mana gasta pelo mago em um turno.
+// O dano será um número aleatório entre o valor do atributo intelligence (dano mínimo) e o valor de intelligence * 2 (dano máximo).
+// A mana consumida por turno é 15. Além disso, a função deve ter uma condicional: caso o mago tenha menos de 15 de mana, o valor de dano recebe uma mensagem (Ex: “Não possui mana suficiente”), e a mana gasta é 0.
+const spentMage = () => {
+  const damageMin = mage.intelligence;
+  const damageMax = Math.floor(Math.random() * (damageMin * 2)) + 1;
+  let damage = damageMax < damageMin ? damageMin : damageMax;
+  let mana = mage.mana - 15;
+  if (mage.mana < 15) {
+    damage = 'Não possui mana suficiente';
+    mana = mage.mana - 0;
+  }
+  return {
+    damage: damage,
+    mana: mana,
+  }
+}
+console.log(spentMage());
