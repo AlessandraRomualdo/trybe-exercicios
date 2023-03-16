24d931a2a32c2bc1c253798edbff594a9e7c3a0d
AlessandraRomualdo <romualdo.alessandra23@gmail.com>
2023-03-16 14:35:46 -0300

    adicionando funcionalidade copiar senha gerada

--- 2-front-end/1-Introdução ao Frontend e JavaScript assíncrono/Dia-1 Ambiente de desenvolvimento/gerador-de-senhas/src/mains.js
+++ 2-front-end/1-Introdução ao Frontend e JavaScript assíncrono/Dia-1 Ambiente de desenvolvimento/gerador-de-senhas/src/mains.js
@@ -1,4 +1,5 @@
 import { nanoid } from 'nanoid';
+import copy  from 'clipboard-copy';
 import './style.css';
 
 const passwordBtn = document.querySelector('button');
@@ -6,4 +7,9 @@ const displayPassword = document.querySelector('h2');
 passwordBtn.addEventListener('click', () => {
     const randomPassword = nanoid();
     displayPassword.innerHTML = randomPassword;
+});
+
+displayPassword.addEventListener('click', () => {
+    copy(event.target.innerHTML);
+    alert('Senha copiada!');
 });
