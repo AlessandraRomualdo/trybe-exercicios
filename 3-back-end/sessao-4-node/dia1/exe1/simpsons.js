2250e461e1ae9ff76fe5bd550e19222455b759fe
AlessandraRomualdo <romualdo.alessandra23@gmail.com>
2023-07-13 17:00:42 -0300

    exercicio 6d

--- 3-back-end/sessao-4-node/dia1/exe1/simpsons.js
+++ 3-back-end/sessao-4-node/dia1/exe1/simpsons.js
@@ -20,11 +20,21 @@ const fs = require('fs').promises;
 // returnSimpson(3);
 
 // 6C
-const removeSimpson = async () => {
+// const removeSimpson = async () => {
+//   const file = await fs.readFile('./simpsons.json', 'utf-8');
+//   const data = JSON.parse(file);
+//   const newSimpson = data.filter((simpson) => Number(simpson.id) !== 10 && Number(simpson.id) !== 6);
+//   await fs.writeFile('./simpsons.json', JSON.stringify(newSimpson));
+//   console.log(newSimpson);
+// };
+// removeSimpson();
+
+// 6D
+const familySimpson = async () => {
   const file = await fs.readFile('./simpsons.json', 'utf-8');
   const data = JSON.parse(file);
-  const newSimpson = data.filter((simpson) => Number(simpson.id) !== 10 && Number(simpson.id) !== 6);
-  await fs.writeFile('./simpsons.json', JSON.stringify(newSimpson));
-  console.log(newSimpson);
+  const family = data.slice(0, 4);
+  console.log(family);
+  await fs.writeFile('./simpsonsFamily.json', JSON.stringify(family));
 };
-removeSimpson();
+familySimpson();
