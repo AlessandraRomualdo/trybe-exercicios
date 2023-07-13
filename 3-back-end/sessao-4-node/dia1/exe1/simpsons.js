9415a88794da29895bc5d27170870e3a16ee7dda
AlessandraRomualdo <romualdo.alessandra23@gmail.com>
2023-07-13 16:48:42 -0300

    exercicos 6a ao 6c

---
+++ 3-back-end/sessao-4-node/dia1/exe1/simpsons.js
@@ -0,0 +1,30 @@
+const fs = require('fs').promises;
+
+// 6A
+// const readSimpsons = async () => {
+//   const file = await fs.readFile('./simpsons.json', 'utf-8');
+//   const data = JSON.parse(file);
+//   const simpsons = data.map(({ id, name }) => `${id} - ${name}`);
+//   simpsons.forEach((simpson) => console.log(simpson));
+// }
+// readSimpsons();
+
+// 6B
+// const returnSimpson = async (id) => {
+//   const file = await fs.readFile('./simpsons.json', 'utf-8');
+//   const data = JSON.parse(file);
+//   const simpson = data.find((simpson) => Number(simpson.id) === id);
+//   if (!simpson) throw new Error('id não encontrado');
+//   console.log(`${simpson.id} - ${simpson.name}`);
+// };
+// returnSimpson(3);
+
+// 6C
+const removeSimpson = async () => {
+  const file = await fs.readFile('./simpsons.json', 'utf-8');
+  const data = JSON.parse(file);
+  const newSimpson = data.filter((simpson) => Number(simpson.id) !== 10 && Number(simpson.id) !== 6);
+  await fs.writeFile('./simpsons.json', JSON.stringify(newSimpson));
+  console.log(newSimpson);
+};
+removeSimpson();
